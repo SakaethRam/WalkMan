cb019c0455a53be330ae6d7d475d6508b46dcc49
R Sakaeth Ram <sakaethrambusiness@gmail.com>
2026-08-15 13:53:10 +0530

    Add files via upload

---
+++ WalkMan-UX/Music Console/script.js
@@ -0,0 +1,24 @@
+document.getElementById("demonSlayer-list").onclick = function() {
+    window.location.href = "/*Provide The Demon Slayer MP Page Path*/";
+  };
+
+  document.getElementById("oshiNK-list").onclick = function() {
+    window.location.href = "/*Provide The Oshi No Ko MP Page Path*/";
+  };
+
+  document.getElementById("bleach-list").onclick = function() {
+    window.location.href = "/*Provide The Bleach MP Page Path*/";
+  };
+
+
+  document.getElementById("demonSlayer-tier").onclick = function() {
+    window.location.href = "/*Provide The Demon Slayer MP Page Path*/";
+  };
+
+  document.getElementById("oshiNK-tier").onclick = function() {
+    window.location.href = "/*Provide The Oshi No Ko MP Page Path*/";
+  };
+
+  document.getElementById("bleach-tier").onclick = function() {
+    window.location.href = "/*Provide The Bleach MP Page Path*/";
+  };
